bb6799bf1b2ce69a92c1af758b761fdadddfccc8
Ali Madooei <madooei@jhu.edu>
2020-11-25 23:09:42 -0500

    Step 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { Component } from "react";
 import ListMeetings from "./ListMeetings.js";
 import { Route, Switch } from "react-router";
+import axios from "axios";
 import CreateMeeting from "./CreateMeeting.js";
 
 class App extends Component {
   state = {
     meetings: [],
   };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:4567/api/meetings")
+      .then((response) => this.setState({ meetings: response.data.data }))
+      .catch((err) => console.log(err));
+  }
 
   removeMeeting = (meeting) => {
     this.setState((state) => {
